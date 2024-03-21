a39528f68bdfa3b447d7a8180dfc37f75760c54b
Linet <teweilinet@gmail.com>
2024-03-21 11:03:14 +0300

    First Class Functions"

---
+++ index.js
@@ -0,0 +1,16 @@
+const receivesAFunction = (callback) => callback()
+
+
+const returnsANamedFunction=(name) => 
+function name() {
+    return name("Mellisa")
+}
+
+const returnsAnAnonymousFunction=(greet) => 
+function () {
+    return greet('')
+}
+
+
+
+ 
